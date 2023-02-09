e5b5b906b20b8c569b6d8b5ce5a763e9be0edb6d
Muhammad Masood <yoursourcecode@gmail.com>
2023-02-08 22:55:58 -0800

    strings

---
+++ Basics/Strings.ts
@@ -0,0 +1,21 @@
+const n = "Masood";
+console.log(`Hello ${n}, would you like to learn
+some Typescript today?\n`);
+
+let n2 = "masood waheed";
+console.log(n2.toUpperCase(),n2.toLowerCase(),"\n");
+
+console.log("Albert Einstein once said, A person who never made a mistake never tried anything new.\n");
+
+let famous_person = "Anounymous";
+let message = "New Message";
+console .log(famous_person,message);
+
+const stripName = "\tMasood\n";
+console.log("Before Stripping: ",stripName);
+console.log("After Stripping: ",stripName.trim(),"\n");
+
+
+
+
+
