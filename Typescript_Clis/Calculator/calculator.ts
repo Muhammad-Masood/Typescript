941219f24fbbf1f68ac9e507dd2db1524a1d87b5
Muhammad Masood <yoursourcecode@gmail.com>
2023-03-27 14:22:48 -0700

    new commit

---
+++ Typescript_Clis/Calculator/calculator.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+import { calc } from "./calc_funcs.js";
+
+async function args(){
+    const arg = await inquirer.prompt([{
+        type:"number",
+        name:"number1",
+        message:"Input Number 1"
+    },
+    {
+        type:"number",
+        name:"number2",
+        message:"Input Number 2"
+    }]);
+    return {number1:arg.number1,number2:arg.number2};
+}
+
+async function calculator() {
+
+let opt = true;
+
+while(opt){
+    const choice = await inquirer.prompt([{
+        type:"list",
+        name:"opt",
+        message:"Choose Operation",
+        choices:["+","-","*","/","%","Exit"]
+    },
+]);
+
+if(choice.opt == "+"){
+    const {number1:n1,number2:n2} = await args();
+    calc.add(n1,n2);
+}
+else if(choice.opt == "-"){
+    const {number1:n1,number2:n2} = await args();
+    calc.sub(n1,n2);
+}
+else if(choice.opt == "*"){
+    const {number1:n1,number2:n2} = await args();
+    calc.multiply(n1,n2);
+}
+else if(choice.opt == "/"){
+    const {number1:n1,number2:n2} = await args();
+    calc.divide(n1,n2);
+}
+else if(choice.opt == "%"){
+    const {number1:n1,number2:n2} = await args();
+    calc.mod(n1,n2);
+}
+else{
+    opt = false;
+}
+}
+}
+
+calculator();
